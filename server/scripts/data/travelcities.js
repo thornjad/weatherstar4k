1cf25912e62aed95167ecbbcef2e885a15909d72
Jade Michael Thornton <jademichael@jmthornton.net>
2025-06-18 17:21:43 -0500

    simplify travel forecast

--- server/scripts/data/travelcities.js
+++ server/scripts/data/travelcities.js
@@ -11,16 +11,6 @@ const TravelCities = [
 		},
 	},
 	{
-		Name: 'Boston',
-		Latitude: 42.3584,
-		Longitude: -71.0598,
-		point: {
-			x: 71,
-			y: 90,
-			wfo: 'BOX',
-		},
-	},
-	{
 		Name: 'Chicago',
 		Latitude: 41.9796,
 		Longitude: -87.9045,
@@ -28,26 +18,6 @@ const TravelCities = [
 			x: 66,
 			y: 77,
 			wfo: 'LOT',
-		},
-	},
-	{
-		Name: 'Cleveland',
-		Latitude: 41.4995,
-		Longitude: -81.6954,
-		point: {
-			x: 83,
-			y: 65,
-			wfo: 'CLE',
-		},
-	},
-	{
-		Name: 'Dallas',
-		Latitude: 32.8959,
-		Longitude: -97.0372,
-		point: {
-			x: 80,
-			y: 109,
-			wfo: 'FWD',
 		},
 	},
 	{
@@ -61,26 +31,6 @@ const TravelCities = [
 		},
 	},
 	{
-		Name: 'Detroit',
-		Latitude: 42.3314,
-		Longitude: -83.0457,
-		point: {
-			x: 66,
-			y: 34,
-			wfo: 'DTX',
-		},
-	},
-	{
-		Name: 'Hartford',
-		Latitude: 41.7637,
-		Longitude: -72.6851,
-		point: {
-			x: 21,
-			y: 54,
-			wfo: 'BOX',
-		},
-	},
-	{
 		Name: 'Houston',
 		Latitude: 29.7633,
 		Longitude: -95.3633,
@@ -91,16 +41,6 @@ const TravelCities = [
 		},
 	},
 	{
-		Name: 'Indianapolis',
-		Latitude: 39.7684,
-		Longitude: -86.158,
-		point: {
-			x: 58,
-			y: 69,
-			wfo: 'IND',
-		},
-	},
-	{
 		Name: 'Los Angeles',
 		Latitude: 34.0522,
 		Longitude: -118.2437,
@@ -108,16 +48,6 @@ const TravelCities = [
 			x: 155,
 			y: 45,
 			wfo: 'LOX',
-		},
-	},
-	{
-		Name: 'Miami',
-		Latitude: 25.7743,
-		Longitude: -80.1937,
-		point: {
-			x: 110,
-			y: 51,
-			wfo: 'MFL',
 		},
 	},
 	{
@@ -141,56 +71,6 @@ const TravelCities = [
 		},
 	},
 	{
-		Name: 'Norfolk',
-		Latitude: 36.8468,
-		Longitude: -76.2852,
-		point: {
-			x: 90,
-			y: 52,
-			wfo: 'AKQ',
-		},
-	},
-	{
-		Name: 'Orlando',
-		Latitude: 28.5383,
-		Longitude: -81.3792,
-		point: {
-			x: 26,
-			y: 68,
-			wfo: 'MLB',
-		},
-	},
-	{
-		Name: 'Philadelphia',
-		Latitude: 39.9523,
-		Longitude: -75.1638,
-		point: {
-			x: 50,
-			y: 76,
-			wfo: 'PHI',
-		},
-	},
-	{
-		Name: 'Pittsburgh',
-		Latitude: 40.4406,
-		Longitude: -79.9959,
-		point: {
-			x: 78,
-			y: 66,
-			wfo: 'PBZ',
-		},
-	},
-	{
-		Name: 'St. Louis',
-		Latitude: 38.6273,
-		Longitude: -90.1979,
-		point: {
-			x: 95,
-			y: 74,
-			wfo: 'LSX',
-		},
-	},
-	{
 		Name: 'San Francisco',
 		Latitude: 37.7749,
 		Longitude: -122.4194,
@@ -208,26 +88,6 @@ const TravelCities = [
 			x: 125,
 			y: 68,
 			wfo: 'SEW',
-		},
-	},
-	{
-		Name: 'Syracuse',
-		Latitude: 43.0481,
-		Longitude: -76.1474,
-		point: {
-			x: 52,
-			y: 99,
-			wfo: 'BGM',
-		},
-	},
-	{
-		Name: 'Tampa',
-		Latitude: 27.9475,
-		Longitude: -82.4584,
-		point: {
-			x: 71,
-			y: 97,
-			wfo: 'TBW',
 		},
 	},
 	{
